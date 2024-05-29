787a437cd83300e37956f0d47c3e7e7b5351f920
rafael <rafael@win-jornada-bal>
2024-05-29 17:25:31 -0300

    Add project files.

---
+++ Fina.Core/Requests/Transactions/GetTransactionsByPeriodRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Fina.Core.Requests.Transactions
+{
+    public class GetTransactionsByPeriodRequest : PagedResponse
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
